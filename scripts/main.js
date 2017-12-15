4b427157cb958daf2e3e782a984cef9111fe616f
Shawn Luo <shawn.luo.qc@gmail.com>
2017-12-15 15:46:39 -0500

    done:Google API, cant return from callback func

--- scripts/main.js
+++ scripts/main.js
@@ -4,32 +4,35 @@
 * */
 
 // sort data by names
-const data = jsonData.sort(function(a,b) {
+const data = jsonData.sort(function (a, b) {
     return a.name.localeCompare(b.name);
 });
 
 // array to save origin and destination addresses
+// let orgArray = [];
+// let dstArray = [];
+const KEY = "&key=AIzaSyDwwjtjmR9oVWDe0JCkWb34RuZD2lvdMHs";
+const URL = "https://maps.googleapis.com/maps/api/distancematrix/json?";
 
 // DOM objects
-let homeBtn = document.getElementById('homeBtn');
-let helpBtn = document.getElementById('helpBtn');
-
-let nameList = document.getElementById('nameList');
-let intro = document.getElementById('intro');
-let calculator = document.getElementById('calculator');
-let table = document.querySelector('table');
-let total = document.getElementById('total');
-let office = document.getElementById('office');
-let sites = document.getElementById('sites');
-let technician = document.getElementById('technician');
+const homeBtn = document.getElementById('homeBtn');
+const helpBtn = document.getElementById('helpBtn');
+const calcBtn = document.getElementById('calculate');
+const nameList = document.getElementById('nameList');
+const intro = document.getElementById('intro');
+const calculator = document.getElementById('calculator');
+const table = document.querySelector('table');
+const total = document.getElementById('total');
+const office = document.getElementById('office');
+const sites = document.getElementById('sites');
+const technician = document.getElementById('technician');
 
 // to show technicians list
 function renderIndex() {
-    for(let i=0, len=data.length; i<len; i++) {
+    for (let i = 0, len = data.length; i < len; i++) {
         updateIndex(data[i].name);
     }
 }
-
 
 function updateIndex(name) {
     let node = document.createElement("li");
@@ -45,69 +48,101 @@ function renderCalc(name) {
     renderUL(office, techData[0].bureau);
     renderUL(sites, techData[0].sites);
     makeDragDrop();
-    document.getElementById('calculate').addEventListener('click', calculer());
-}
-
-
-function renderUL(objDOM, data) {
-    while (objDOM.hasChildNodes()) {
-        objDOM.removeChild(objDOM.lastChild);
-    }
-    data.sort();
-    for(let i=0, len = data.length; i<len; i++) {
-        let node = document.createElement('li');
-        node.textContent = data[i];
-        objDOM.appendChild(node);
-    }
 }
 
 function makeDragDrop() {
     $('li').draggable({
         helper: "clone", revert: "invalid",
         start: function (e, ui) {
-            ui.helper.animate({width:"15%"},200);
+            ui.helper.animate({width: "150px"}, 200);
         },
-        cursorAt: {left:40, top:25}
-     });
+        cursorAt: {left: 40, top: 25}
+    });
     $('td').droppable({
-        drop: function(e, ui) {
-            $(this).find("input").val( $(ui.draggable).text() );
+        drop: function (e, ui) {
+            $(this).find("input").val($(ui.draggable).text());
         }
     });
 }
 
-//to retrieve table data & save to array
-function getAdds(){
-    // let org=[]
-    // let dst=[]
-    //     let origin, dest;
+function renderUL(objDOM, data) {
+    while (objDOM.hasChildNodes()) {
+        objDOM.removeChild(objDOM.lastChild);
+    }
+    data.sort();
+    for (let i = 0, len = data.length; i < len; i++) {
+        let node = document.createElement('li');
+        node.textContent = data[i];
+        objDOM.appendChild(node);
+    }
+}
 
-    // console.log(table.length);
+//to retrieve table data & save to array
+function getAdds() {
+    let orgArray = [], dstArray = [];
     let len = table.rows.length;
-    for(let i=1; i<len-1; i++) {
+    for (let i = 1; i < len - 1; i++) {
         org = table.rows[i].cells[1].children[0].value;
         dst = table.rows[i].cells[2].children[0].value;
-        if(org && dst) {        console.log(org);console.log(dst);}
-        // if(origin && dest){ org.push(origin); dst.push(dest) };
-        // console.log(table.row[i].cells[]);
+        if (org && dst) {
+            orgArray.push(org);
+            dstArray.push(dst);
+        }
     }
+    return {
+        origs: orgArray,
+        dests: dstArray
+    };
+}
+
+function getDistance(origin,destination, distance) {
+    let service = new google.maps.DistanceMatrixService();
+    let dist = null;
+    service.getDistanceMatrix(
+        {
+            origins: [origin],
+            destinations: [destination],
+            travelMode: 'DRIVING',
+            avoidHighways: false,
+            avoidTolls: false
+        }, function (response, status) {
+            if (status == google.maps.DistanceMatrixStatus.OK) {
+
+                console.log(JSON.stringify(response));
+                console.log(response.rows[0].elements[0].distance.text);
+                dist = response.rows[0].elements[0].distance.text;
+
+                // //console.log(rslt);
+                // return dst.substr(0,dst.indexOf(' '));
+            } else {
+                alert('Error: ' + status);
+            }
+            distance = dist.substr(0,dist.indexOf(' '));
+        });
 
 }
 
-
-function calculer() {
+function calculate() {
 
     //colect table input and save to from[], to[]
-    getAdds();
-    //send request to google api
-    //handle response and show data at page
-    // calculate & show total
+    let adds = getAdds();
+    let distances = []; let total = 0; let dist = 0;
+    let org = adds['origs'], dst = adds['dests'];
+    for(let i=0, len=org.length; i<len; i++) {
+        let temp = getDistance(org[i], dst[i], dist);
+        console.log(temp);
+        //console.log(dist.substr(0,dist.indexOf(' ')));
+         //console.log(distance);
+        //total +=  Number(dist.substr(0,dist.indexOf(' ')));
+        // distances.push(distance);
+    }
+    console.log(total);
 }
 
 window.onload = renderIndex();
-for(let i=0, len=nameList.children.length; i<len; i++) {
+for (let i = 0, len = nameList.children.length; i < len; i++) {
     let obj = nameList.children[i];
-    obj.addEventListener('click', function() {
+    obj.addEventListener('click', function () {
         obj.style.backgroundColor = "Cornsilk";
         obj.style.color = "purple";
         nameList.style.display = "none";
@@ -116,12 +151,19 @@ for(let i=0, len=nameList.children.length; i<len; i++) {
 }
 
 // MENU & BUTTON
-homeBtn.addEventListener('click', function() {
+homeBtn.addEventListener('click', function () {
     calculator.style.display = "none";
     intro.style.display = "block";
     nameList.style.display = "block";
 });
 
-helpBtn.addEventListener('click', () =>{alert('Function TO DO')});
-
-document.getElementById('reset').addEventListener('click', function() {total.textContent = ''});
+helpBtn.addEventListener('click', () => {
+    alert('Function TO DO')
+});
+calcBtn.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    calculate();
+});
+document.getElementById('reset').addEventListener('click', function () {
+    total.textContent = ''
+});
